aea0d422fef75940f3968d76641e6474e65c3c02
vanessa <vanessa@10.17.13.68>
2018-04-17 20:34:56 -0500

    Comitting Comments to context, listeners, and managers

--- src/Refrigerator/FridgeDoorCloseManager.java
+++ src/Refrigerator/FridgeDoorCloseManager.java
@@ -3,7 +3,16 @@ package Refrigerator;
 import java.util.EventListener;
 
 import javax.swing.event.EventListenerList;
-
+/**
+ * Manager class for the FridgeDoorClose event. 
+ * In charge of maintaining list of states that are listeners to 
+ * event of threshold temperature being reached.
+ * 
+ * When FridgeDoorClose manager is called when the event occurs, 
+ * FridgeDoorCloseManager notifies the list of listeners by calling
+ * their appropriate doorClosed() method to carry out 
+ * an appropriate action based on the state that is listening 
+ * */
 public class FridgeDoorCloseManager {
 	private EventListenerList listenerList = new EventListenerList();
 	private static FridgeDoorCloseManager instance;
@@ -21,14 +30,28 @@ public class FridgeDoorCloseManager {
 		return instance;
 	}
 	
+	/**
+	 * Called to register a listener to the FridgeDoorCloseManager.
+	 * Is called when state is 'initialized' when it calls its run() method 
+	 * */
 	public void addDoorCloseListener(FridgeDoorCloseListener listener) {
 		listenerList.add(FridgeDoorCloseListener.class, listener);
 	}
 	
+	/**
+	 * Called to unregister a listener to the FridgeDoorCloseManager.
+	 * Is called when a state transitions to another one, and calls its leave()
+	 * method
+	 * */
 	public void removeDoorCloseListener(FridgeDoorCloseListener listener) {
 		listenerList.remove(FridgeDoorCloseListener.class, listener);
 	}
 	
+	/**
+	 * Called when FridgeDoorClose event occurs.
+	 * Is called when state event occurs and manager notifies all listeners by calling
+	 * their doorClosed() method to take appropriate action 
+	 * */
 	public void processEvent(FridgeDoorCloseEvent event) {
 		EventListener[] listeners = listenerList.getListeners(FridgeDoorCloseListener.class);
 		for (int i = 0; i < listeners.length; i++) {
--- src/Refrigerator/FridgeThresholdReachedListener.java
+++ src/Refrigerator/FridgeThresholdReachedListener.java
@@ -1,7 +1,12 @@
 package Refrigerator;
 
 import java.util.EventListener;
-
+/**
+ * Listener class for the FridgeThresholdReached event. When 
+ * FridgeThresholdReaches manager is called when the event occurs,
+ *  classes that implement this listener interface call their fridgeThresholdReached() 
+ *  function to execute an appropriate action 
+ * */
 public interface FridgeThresholdReachedListener extends EventListener{
 	public void fridgeThresholdReached(FridgeThresholdReachedEvent event);
 
